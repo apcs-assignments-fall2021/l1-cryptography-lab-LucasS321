6133f276132740f3eac59595d68c0e82e3bb157b
lucass <24lspeier@athenian.org>
2021-09-19 22:54:28 -0700

    xzvxcv

--- src/main/java/Affine.java
+++ src/main/java/Affine.java
@@ -2,13 +2,59 @@ import java.util.Scanner;
 
 public class Affine {
     public static String encryptAffine(String message) {
+        String newstr = "";
+        for (int i = 0; i < message.length();i++) {
+            char LTT = message.charAt(i);
+            char LTT2 = message.charAt(i);
+            if (LTT >= 'a' && LTT <= 'z') {
+                LTT = (char) (LTT-'a');
+            }
+            if (LTT >= 'A' && LTT <= 'Z') {
+                LTT = (char) (LTT-'A');
+            }
+            int newnum = ((3 * LTT) ) % 26;
+
+            System.out.println(newnum);
+            if (LTT2 >= 'a' && LTT2 <= 'z') {
+                newstr = newstr + (char) (newnum+'a');
+            } else if (LTT2 >= 'A' && LTT2 <= 'Z') {
+                newstr = newstr+(char) (newnum+'A');
+            } else {
+                newstr = newstr + LTT2;
+            }
+
+        }
+
         // REPLACE THIS WITH YOUR CODE
-        return message;
+        return newstr;
     }
 
     public static String decryptAffine(String message) {
+        String newstr = "";
+        for (int i = 0; i < message.length();i++) {
+            char LTT = message.charAt(i);
+            char LTT2 = message.charAt(i);
+            if (LTT >= 'a' && LTT <= 'z') {
+                LTT = (char) (LTT-'a');
+            }
+            if (LTT >= 'A' && LTT <= 'Z') {
+                LTT = (char) (LTT-'A');
+            }
+            int newnum = ((3 * LTT) ) % 26;
+
+            System.out.println(newnum);
+            if (LTT2 >= 'a' && LTT2 <= 'z') {
+                newstr = newstr + (char) (newnum+'a');
+            } else if (LTT2 >= 'A' && LTT2 <= 'Z') {
+                newstr = newstr+(char) (newnum+'A');
+            } else {
+                newstr = newstr + LTT2;
+            }
+
+        }
+
         // REPLACE THIS WITH YOUR CODE
-        return message;
+        return newstr;
     }
 
     public static String encryptAffineKeys(String message, int key1, int key2) {
--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -5,32 +5,124 @@ public class Caesar {
     // and returns the encrypted String
     // Ex. encryptCaesar("Hello, World!") => "Khoor, Zruog!"
     public static String encryptCaesar(String message) {
+        String newstr = "";
+        for (int i = 0; i < message.length();i++) {
+            char LTT = message.charAt(i);
+            if ((LTT >= 'a' && LTT <= 'z') || (LTT >= 'A' && LTT <= 'Z')) {
+                if (LTT == 'Z') {
+                    newstr = newstr+"C";
+                } else if (LTT == 'Y') {
+                    newstr=  newstr+"B";
+                }
+                else if (LTT == 'X') {
+                    newstr=  newstr+"A";
+                } else if (LTT == 'z') {
+                    newstr=  newstr+"c";
+                } else if (LTT == 'y') {
+                    newstr=  newstr+"b";
+                } else if (LTT == 'x') {
+                    newstr=  newstr+"a";
+                } else {
+                    newstr = newstr+(char) (LTT+3);
+                }
+            } else {
+                newstr=newstr+LTT;
+            }
+
+        }
         // REPLACE THIS WITH YOUR CODE
-        return message;
+        return newstr;
     }
 
     // Given a String, decrypts the String with the Caesar cipher
     // and returns the original String
     // Ex. decryptCaesar("Khoor, Zruog!") => "Hello, World!"
     public static String decryptCaesar(String message) {
+        String newstr = "";
+        for (int i = 0; i < message.length();i++) {
+            char LTT = message.charAt(i);
+            if ((LTT >= 'a' && LTT <= 'z') || (LTT >= 'A' && LTT <= 'Z')) {
+                if (LTT == 'A') {
+                    newstr = newstr+"X";
+                } else if (LTT == 'B') {
+                    newstr=  newstr+"Y";
+                }
+                else if (LTT == 'C') {
+                    newstr=  newstr+"Z";
+                } else if (LTT == 'a') {
+                    newstr=  newstr+"x";
+                } else if (LTT == 'b') {
+                    newstr=  newstr+"y";
+                } else if (LTT == 'c') {
+                    newstr=  newstr+"z";
+                } else {
+                    newstr = newstr+(char) (LTT-3);
+                }
+            } else {
+                newstr=newstr+LTT;
+            }
+
+        }
         // REPLACE THIS WITH YOUR CODE
-        return message;
+        return newstr;
     }
 
     // Given a String and a key corresponding to a shift, encrypts
     // the String using the given key and returns the encrypted String
     // Ex. encryptCaesarKey("Hello, World!", 5) => "Mjqqt, Btwqi!".
     public static String encryptCaesarKey(String message, int key) {
+        String newstr = "";
+        for (int i = 0; i < message.length();i++) {
+            char LTT = message.charAt(i);
+            if ((LTT >= 'a' && LTT <= 'z') || (LTT >= 'A' && LTT <= 'Z')) {
+                int NewKey =  (LTT + key);
+                if ((LTT >= 'A' && LTT <= 'Z')) {
+                    while (!((NewKey >= 'A' && NewKey <= 'Z'))) {
+                        NewKey = NewKey - (26);
+                    }
+                }
+                if ((LTT >= 'a' && LTT <= 'z')) {
+                    while (!((NewKey >= 'a' && NewKey <= 'z'))) {
+                        NewKey = NewKey - (26);
+                    }
+                }
+                newstr = newstr + (char) NewKey;
+            } else {
+                newstr=newstr+LTT;
+            }
+
+        }
         // REPLACE THIS WITH YOUR CODE
-        return message;
+        return newstr;
     }
 
     // Given a String and a key corresponding to a shift, decrypts
     // the String using the given key and returns the original String
     // Ex. decryptCaesarKey("Mjqqt, Btwqi!", 5) => "Hello, World!"
     public static String decryptCaesarKey(String message, int key) {
+        String newstr = "";
+        for (int i = 0; i < message.length();i++) {
+            char LTT = message.charAt(i);
+            if ((LTT >= 'a' && LTT <= 'z') || (LTT >= 'A' && LTT <= 'Z')) {
+                int NewKey =  (LTT - key);
+                if ((LTT >= 'A' && LTT <= 'Z')) {
+                    while (!((NewKey >= 'A' && NewKey <= 'Z'))) {
+                        NewKey = NewKey + (26);
+                    }
+                }
+                if ((LTT >= 'a' && LTT <= 'z')) {
+                    while (!((NewKey >= 'a' && NewKey <= 'z'))) {
+                        NewKey = NewKey + (26);
+                    }
+                }
+                newstr = newstr + (char) NewKey;
+            } else {
+                newstr=newstr+LTT;
+            }
+
+        }
         // REPLACE THIS WITH YOUR CODE
-        return message;
+        return newstr;
     }
 
 
